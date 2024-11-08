739f2b0c1d1b81483697acd100f5327d096001fd
Lee Shavit <lee.shavit10@gmail.com>
2024-11-08 22:09:46 +0200

    Player basic functionality and design

--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -2,7 +2,8 @@ import {  useRef , useState } from 'react'
 
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { SongControls } from './SongControls.jsx'
+import { PlayerControls } from './PlayerControls.jsx'
+
 
 export function SongPlayer() {
     // const nowPlayingSong = useSelector(storeState => storeState.Player.nowPlayingSong)
@@ -12,7 +13,7 @@ export function SongPlayer() {
 
     const nowPlayingSong = {
         id: '3xKsf9qdS1CyvXSMEid6g8',
-        title: "Pink+White",
+        name: "Pink+White",
         artists: [
             {
                 name: 'Frank Ocean',
@@ -46,49 +47,56 @@ export function SongPlayer() {
             playerRef.current.setVolume(newVolume)
       }
 
-    const { imgUrl, title, album, artists, _id } = nowPlayingSong
-
+    const { imgUrl, title, album, artists, _id, name } = nowPlayingSong
     return (
         <section className="player full">
-            <div className='song-details'>
+            <div className='song-info'>
                 <img src={imgUrl}></img>
-                <div>
+                <div className='song-info-details'>
                     {/* <Link to={`/album/${nowPlayingSong.album._id}`}/> */}
                     {/* <Link to={`/artist/${nowPlayingSong.._id}`}/> */}
-                    <p>{title}</p>
-                    <p>{artists.join(', ')}</p>
+                    <p className='song-info-name'>{title}</p>
+                    <p className='song-info-artist'>{artists.join(', ')}</p>
                 </div>
                 <button onClick={() => onAddToLikedSongs(_id)}>
-                    <img src="src/assets/styles/img/like-icon-like.svg" alt="" />
+                    <img src="/src/assets/img/like-icon-like-icon.svg" alt="" />
                 </button>
             </div>
-            <SongControls videoId={_id} playerRef={playerRef} volume={volume}/>
-            <div className='player-btns'>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+            <PlayerControls songName={name} playerRef={playerRef} volume={volume}/>
+            <div className='player-buttons'>
+                <button>
+                <img src='/src/assets/img/now-playing-view-icon.svg' alt='Volume'/>
+                </button>
+                <button>
+                <img src='/src/assets/img/lyrics-icon.svg' alt='Volume'/>
+                </button>
+                <button>                
+                    <img src='/src/assets/img/queue-icon.svg' alt='Volume'/>
+                </button>
+                <button>
+                <img src='/src/assets/img/connect-to-device-icon.svg' alt='Volume'/>
+                </button>
                 {/* Volume */}
-                <div className="flex items-center gap-2 justify-end">
-                    <button className="text-[#b3b3b3] hover:text-white p-2 transition-colors">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                            <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z" />
-                            <path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z" />
-                        </svg>
+                <div className="volume">
+                    <button>
+                        <img src='/src/assets/img/volume-mid-icon.svg' alt='Volume'/>
                     </button>
                     <div
-                        className="w-[100px] h-1 bg-[#404040] rounded-full cursor-pointer relative group"
+                        className="volume-slider"
                         onClick={handleVolumeClick}
                     >
                         <div
-                            className="absolute left-0 top-0 h-full bg-white group-hover:bg-[#1db954] rounded-full transition-colors"
+                            className="volume-slider-fill"
                             style={{ width: `${volume}%` }}
                         />
                     </div>
                 </div>
-                <button></button>
-                <button></button>
+                <button>
+                <img src="/src/assets/img/open-miniplayer-icon.svg"/>
+                </button>
+                <button>
+                    <img src="/src/assets/img/full-screen-icon.svg"/>
+                </button>
             </div>
         </section>
     )
