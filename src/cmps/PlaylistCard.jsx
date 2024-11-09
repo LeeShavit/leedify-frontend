c9336a8dd66615c8476106d2348c1f82ec025a19
1id0r <lidornissim44@gmail.com>
2024-11-09 20:57:22 +0200

    Add explore page

--- src/cmps/PlaylistCard.jsx
+++ src/cmps/PlaylistCard.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import { Play } from 'lucide-react'
 
-export default function PlaylistCard({ imageUrl, title, description, aspectSquare = true }) {
+export function PlaylistCard({ station }) {
   return (
-    <div className='playlist-card'>
+    <Link to={`/station/${station._id}`} className='playlist-card'>
       <div className='playlist-card__image-container'>
-        <img
-          src={imageUrl || 'https://i.scdn.co/image/ab67616d0000b273491678beaffcefac517a699e'}
-          alt={title}
-          className={aspectSquare ? '' : 'aspect-[3/4]'}
-        />
+        <img src={station.imgUrl} alt={station.name} />
         <button className='playlist-card__play-button' aria-label='Play'>
           <Play size={24} color='black' fill='black' />
         </button>
       </div>
 
-      <h3 className='playlist-card__title'>{title}</h3>
-      {description && <p className='playlist-card__description'>{description}</p>}
-    </div>
+      <h3 className='playlist-card__title'>{station.name}</h3>
+      {station.description && <p className='playlist-card__description'>{station.description}</p>}
+    </Link>
   )
 }
--- src/services/station/station.service.local.js
+++ src/services/station/station.service.local.js
@@ -1,6 +1,7 @@
+// services/station.service.js
 import { storageService } from '../async-storage.service'
-
-const STORAGE_KEY = 'stations'
+import { makeId } from '../util.service'
+const STORAGE_KEY = 'stations_db'
 
 export const stationService = {
   query,
@@ -10,46 +11,96 @@ export const stationService = {
   getEmptyStation,
   addSongToStation,
   removeSongFromStation,
-  createDefaultStations,
 }
 
+_createDemoData()
+
 async function query(filterBy = {}) {
-  let stations = await storageService.query(STORAGE_KEY)
-  if (!stations || !stations.length) {
-    stations = await createDefaultStations()
-    await storageService.post(STORAGE_KEY, stations)
-  }
+  try {
+    let stations = await storageService.query(STORAGE_KEY)
 
-  // Apply filters
-  if (filterBy.txt) {
-    const regex = new RegExp(filterBy.txt, 'i')
-    stations = stations.filter(
-      (station) =>
-        regex.test(station.name) || regex.test(station.description) || station.tags.some((tag) => regex.test(tag))
-    )
-  }
-  if (filterBy.tag) {
-    stations = stations.filter((station) => station.tags.includes(filterBy.tag))
-  }
+    if (filterBy.txt) {
+      const regex = new RegExp(filterBy.txt, 'i')
+      stations = stations.filter(
+        (station) =>
+          regex.test(station.name) || regex.test(station.description) || station.tags.some((tag) => regex.test(tag))
+      )
+    }
+    if (filterBy.tag) {
+      stations = stations.filter((station) => station.tags.includes(filterBy.tag))
+    }
 
-  return stations
+    return stations
+  } catch (err) {
+    console.error("station service - couldn't get stations", err)
+    throw err
+  }
 }
 
 async function getById(stationId) {
-  return await storageService.get(STORAGE_KEY, stationId)
+  try {
+    return await storageService.get(STORAGE_KEY, stationId)
+  } catch (err) {
+    console.error("station service - couldn't get station", err)
+    throw err
+  }
 }
 
 async function remove(stationId) {
-  return await storageService.remove(STORAGE_KEY, stationId)
+  try {
+    return await storageService.remove(STORAGE_KEY, stationId)
+  } catch (err) {
+    console.error("station service - couldn't remove station", err)
+    throw err
+  }
 }
 
 async function save(station) {
-  if (station._id) {
-    return await storageService.put(STORAGE_KEY, station)
-  } else {
-    // Create new station
-    station.createdAt = Date.now()
-    return await storageService.post(STORAGE_KEY, station)
+  try {
+    if (station._id) {
+      return await storageService.put(STORAGE_KEY, station)
+    } else {
+      return await storageService.post(STORAGE_KEY, station)
+    }
+  } catch (err) {
+    console.error("station service - couldn't save station", err)
+    throw err
+  }
+}
+
+async function addSongToStation(stationId, song) {
+  try {
+    const station = await getById(stationId)
+    if (!station) throw new Error(`Station ${stationId} not found`)
+
+    const songExists = station.songs.some((s) => s.id === song.id)
+    if (songExists) return station
+
+    const songToAdd = {
+      ...song,
+      addedAt: Date.now(),
+    }
+    station.songs.push(songToAdd)
+    return await save(station)
+  } catch (err) {
+    console.error("station service - couldn't add song to station", err)
+    throw err
+  }
+}
+
+async function removeSongFromStation(stationId, songId) {
+  try {
+    const station = await getById(stationId)
+    if (!station) throw new Error(`Station ${stationId} not found`)
+
+    const songIdx = station.songs.findIndex((song) => song.id === songId)
+    if (songIdx === -1) return station
+
+    station.songs.splice(songIdx, 1)
+    return await save(station)
+  } catch (err) {
+    console.error("station service - couldn't remove song from station", err)
+    throw err
   }
 }
 
@@ -65,132 +116,95 @@ function getEmptyStation() {
   }
 }
 
-async function addSongToStation(stationId, song) {
-  const station = await getById(stationId)
-  if (!station) throw new Error(`Station ${stationId} not found`)
+// Private functions
+function _createDemoData() {
+  const stations = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  if (stations.length > 0) return
 
-  // Check if song already exists
-  const songExists = station.songs.some((s) => s.id === song.id)
-  if (songExists) return station
-
-  // Add song with metadata
-  const songToAdd = {
-    ...song,
-    addedAt: Date.now(),
-  }
-  station.songs.push(songToAdd)
-  return await save(station)
-}
-
-async function removeSongFromStation(stationId, songId) {
-  const station = await getById(stationId)
-  if (!station) throw new Error(`Station ${stationId} not found`)
-
-  const songIdx = station.songs.findIndex((song) => song.id === songId)
-  if (songIdx === -1) return station
-
-  station.songs.splice(songIdx, 1)
-  return await save(station)
-}
-
-async function createDefaultStations() {
-  const stations = [
+  const demoStations = [
     {
-      _id: utilService.makeId(),
-      name: 'Karol G Top Hits',
-      description: 'The best songs from Karol G',
-      imgUrl: 'https://i.scdn.co/image/ab67616d0000b273491678beaffcefac517a699e',
-      tags: ['Latin', 'Reggaeton'],
-      createdBy: {
-        _id: 'u101',
-        fullname: 'Admin',
-        imgUrl: '',
-      },
-      likedByUsers: [],
-      songs: _createKarolGSongs(),
-    },
-    {
-      _id: utilService.makeId(),
-      name: 'Pink Floyd Classics',
-      description: 'Timeless classics from Pink Floyd',
+      _id: makeId(),
+      name: 'Pink Floyd Essentials',
+      description: 'Best of Pink Floyd',
       imgUrl: 'https://i.scdn.co/image/ab67616d0000b273ea7caaff71dea1051d49b2fe',
-      tags: ['Rock', 'Progressive Rock'],
+      tags: ['Rock', 'Psychedelic'],
       createdBy: {
         _id: 'u101',
         fullname: 'Admin',
         imgUrl: '',
       },
       likedByUsers: [],
-      songs: _createPinkFloydSongs(),
+      songs: [
+        {
+          id: '4gMgiXfqyzZLMhsksGmbQV',
+          title: 'Another Brick in the Wall, Pt. 2',
+          artistName: 'Pink Floyd',
+          albumName: 'The Wall',
+          albumId: '5Dbax7G8SWrP9xyzkOvy2F', // Spotify Album ID
+          duration: 238746,
+          imgUrl: 'https://i.scdn.co/image/ab67616d0000b2735d48e2f56d691f9a4e4b0bdf',
+          addedAt: Date.now(),
+          uri: 'spotify:track:4gMgiXfqyzZLMhsksGmbQV', // Spotify URI
+          preview_url: 'https://p.scdn.co/mp3-preview/73d913b1a9cfa64fda1f7d04d7bb16345fa0aac4',
+        },
+        {
+          id: '6mFkJmJqdDVQ1REhVfGgd1', // Spotify ID
+          title: 'Wish You Were Here',
+          artistName: 'Pink Floyd',
+          albumName: 'Wish You Were Here',
+          albumId: '0bCAjiUamIFqKJsekOYuRw', // Spotify Album ID
+          duration: 334743,
+          imgUrl: 'https://i.scdn.co/image/ab67616d0000b2731a84d71391df7469c5ab8539',
+          addedAt: Date.now(),
+          uri: 'spotify:track:6mFkJmJqdDVQ1REhVfGgd1', // Spotify URI
+          preview_url: 'https://p.scdn.co/mp3-preview/e3d046771206da9115d0a619ede2210b610dc9f0',
+        },
+      ],
     },
     {
-      _id: utilService.makeId(),
-      name: 'Romeo Santos Hits',
-      description: "The king of Bachata's greatest hits",
-      imgUrl: 'https://i.scdn.co/image/ab67616d0000b273e9da42890bbd629df1e8f640',
-      tags: ['Bachata', 'Latin'],
+      _id: makeId(),
+      name: 'Latin Hits',
+      description: 'Top Latin songs',
+      imgUrl: 'https://i.scdn.co/image/ab67616d0000b273491678beaffcefac517a699e',
+      tags: ['Latin', 'Pop'],
       createdBy: {
         _id: 'u101',
         fullname: 'Admin',
         imgUrl: '',
       },
       likedByUsers: [],
-      songs: _createRomeoSantosSongs(),
+      songs: [
+        {
+          id: '54zcJnb3tp9c5OVKREZ1Is',
+          title: 'MI EX TENÍA RAZÓN',
+          artistName: 'KAROL G',
+          albumName: 'MAÑANA SERÁ BONITO (BICHOTA SEASON)',
+          albumId: '0FqAaUEyKCyUNFE1uQPZ7i',
+          duration: 154374,
+          imgUrl: 'https://i.scdn.co/image/ab67616d0000b273d026bf9d7780f6a1267b4d03',
+          addedAt: Date.now(),
+          uri: 'spotify:track:54zcJnb3tp9c5OVKREZ1Is',
+          preview_url: null,
+        },
+        {
+          id: '5PycBIeabfvX3n9ILG7Vrv',
+          title: 'Propuesta Indecente',
+          artistName: 'Romeo Santos',
+          albumName: 'Fórmula, Vol. 2 (Deluxe Edition)',
+          albumId: '17HsiXfqKUPoTP6Y5ebs1L',
+          duration: 235133,
+          imgUrl: 'https://i.scdn.co/image/ab67616d0000b273e9da42890bbd629df1e8f640',
+          addedAt: Date.now(),
+          uri: 'spotify:track:5PycBIeabfvX3n9ILG7Vrv',
+          preview_url: 'https://p.scdn.co/mp3-preview/517abecfde814f6ecb4459b4d2ff4c250ed80ec5',
+        },
+      ],
     },
   ]
 
-  return stations
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(demoStations))
 }
 
-function _createKarolGSongs() {
-  const karolGData = require('../data/karolgtopsongs.json')
-  return karolGData.tracks.map((track) => ({
-    id: track.id,
-    title: track.name,
-    artistName: track.artists.map((artist) => artist.name).join(', '),
-    albumName: track.album.name,
-    albumId: track.album.id,
-    duration: track.duration_ms,
-    imgUrl: track.album.images[0].url,
-    addedAt: Date.now(),
-    createdAt: new Date(track.album.release_date).getTime(),
-    url: track.preview_url || null,
-  }))
-}
-
-function _createPinkFloydSongs() {
-  const pinkFloydData = require('../data/topUs10songs.json')
-  return pinkFloydData.tracks.map((track) => ({
-    id: track.id,
-    title: track.name,
-    artistName: track.artists.map((artist) => artist.name).join(', '),
-    albumName: track.album.name,
-    albumId: track.album.id,
-    duration: track.duration_ms,
-    imgUrl: track.album.images[0].url,
-    addedAt: Date.now(),
-    createdAt: new Date(track.album.release_date).getTime(),
-    url: track.preview_url || null,
-  }))
-}
-
-function _createRomeoSantosSongs() {
-  const romeoSantosData = require('../data/romeoSantos.json')
-  return romeoSantosData.tracks.map((track) => ({
-    id: track.id,
-    title: track.name,
-    artistName: track.artists.map((artist) => artist.name).join(', '),
-    albumName: track.album.name,
-    albumId: track.album.id,
-    duration: track.duration_ms,
-    imgUrl: track.album.images[0].url,
-    addedAt: Date.now(),
-    createdAt: new Date(track.album.release_date).getTime(),
-    url: track.preview_url || null,
-  }))
-}
-
-// Helper function to format song duration from milliseconds
 function _formatDuration(ms) {
   const minutes = Math.floor(ms / 60000)
   const seconds = ((ms % 60000) / 1000).toFixed(0)
