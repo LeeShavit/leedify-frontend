8b6a2200cbbe3f1696794a56ed8b75090a5f1a4a
1id0r <lidornissim44@gmail.com>
2024-11-11 10:42:04 +0200

    Commit- before work

--- src/store/actions/station.actions.js
+++ src/store/actions/station.actions.js
@@ -25,6 +25,7 @@ export async function loadStations(filterBy) {
 export async function loadStation(stationId) {
   try {
     const station = await stationService.getById(stationId)
+    console.log('station', station)
     store.dispatch(getCmdSetStation(station))
     return station
   } catch (err) {
