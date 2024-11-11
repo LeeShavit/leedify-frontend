2ef0674076f8550b8251335949eda5f02134ebf2
Lee Shavit <lee.shavit10@gmail.com>
2024-11-11 10:37:51 +0200

    more design for add-song

--- src/assets/img/app-header/icons.jsx
+++ src/assets/img/app-header/icons.jsx
@@ -56,3 +56,10 @@ export const BellIcon = ({ className }) => (
     ></path>
   </svg>
 )
+
+export const xIcon = ({ className }) => (
+  <svg data-encore-id="icon" role="img" aria-label="Close" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 eJsVCw">
+    <path d="M3.293 3.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 0 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414z"
+    ></path>
+    </svg>
+)
