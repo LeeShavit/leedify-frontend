f115f8c04c9e1f76de0ee29caa77e285ae7055d6
1id0r <lidornissim44@gmail.com>
2024-11-14 17:22:45 +0200

    Login user , cred not fully worked

--- src/cmps/Library.jsx
+++ src/cmps/Library.jsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-import { ArrowRightIcon, LibraryIcon, LibraryIconFull, LibrarySearchIcon, ListIcon, PlusIcon } from '../assets/img/library/icons'
+import {
+  ArrowRightIcon,
+  LibraryIcon,
+  LibraryIconFull,
+  LibrarySearchIcon,
+  ListIcon,
+  PlusIcon,
+} from '../assets/img/library/icons'
 import { PlayIcon } from 'lucide-react'
 import { Button } from '@mui/material'
 import { PauseIcon } from '../assets/img/player/icons'
@@ -13,12 +20,11 @@ import { addToQueue, playNext, setIsPlaying } from '../store/actions/player.acti
 import { stationService } from '../services/station/station.service.local'
 
 export function Library() {
-
   const [selectedTab, setSelectedTab] = useState('playlists')
   const [isExpanded, setIsExpanded] = useState(true)
 
-  const stations = useSelector(state => state.stationModule.stations)
-  const user = useSelector(state => state.userModule.user)
+  const stations = useSelector((state) => state.stationModule.stations)
+  const user = useSelector((state) => state.userModule.user)
   const currentStationId = useSelector((state) => state.playerModule.currentStationId)
   const isPlaying = useSelector((state) => state.playerModule.isPlaying)
 
@@ -28,10 +34,9 @@ export function Library() {
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
 
-  useEffect(()=>{
+  useEffect(() => {
     loadStations()
-  },[])
-
+  }, [])
 
   async function handleCreatePlaylist() {
     try {
@@ -55,7 +60,10 @@ export function Library() {
 
   async function onPlayStation(stationId) {
     try {
-      const station = (stationId === 'liked-songs') ? await stationService.getLikedSongsStation() : await stationService.getById(stationId)
+      const station =
+        stationId === 'liked-songs'
+          ? await stationService.getLikedSongsStation()
+          : await stationService.getById(stationId)
       addToQueue(station.songs, stationId)
       playNext()
       setIsPlaying(true)
@@ -111,24 +119,28 @@ export function Library() {
             className='sort-btn'
             onClick={handleClick}
             aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
+            aria-haspopup='true'
             aria-expanded={open ? 'true' : undefined}
-            sx={{ textTransform: 'none', fontFamily: 'Spotify-mix, sans-serif' }}>
+            sx={{ textTransform: 'none', fontFamily: 'Spotify-mix, sans-serif' }}
+          >
             Recently Added
             <ListIcon className='list-icon' sx={{ fontSize: '24px', opacity: 0.7 }} />
           </Button>
           <LibrarySortMenu
-            id="basic-menu"
+            id='basic-menu'
             anchorEl={anchorEl}
             open={open}
             onClose={handleClose}
-            MenuListProps={{ 'aria-labelledby': 'basic-button', }}
+            MenuListProps={{ 'aria-labelledby': 'basic-button' }}
           />
         </div>
       </div>
 
       <div className='library-list'>
-        <div className={`library-item liked-songs ${(currentStationId === 'liked-songs') && 'current-station'}`} onClick={() => onNavigateToStation('liked-songs')}>
+        <div
+          className={`library-item liked-songs ${currentStationId === 'liked-songs' && 'current-station'}`}
+          onClick={() => onNavigateToStation('liked-songs')}
+        >
           <button className='library-item__image-button' onClick={() => onPlayStation('liked-songs')}>
             <img src='https://misc.scdn.co/liked-songs/liked-songs-300.png' alt='liked-songs' />
             <div className='library-item__image-overlay'>
@@ -139,16 +151,31 @@ export function Library() {
             <h3 className='library-item__title'>Liked Songs</h3>
             <p className='library-item__details'>
               <span className='playlist-tag'>Playlist</span>
-              <span>{`${user?.likedSongs.length} songs`}</span>
+              <span>
+                <span>
+                  {!user?.likedSongs
+                    ? 'No songs yet'
+                    : user.likedSongs.length === 0
+                    ? 'No songs yet'
+                    : `${user.likedSongs.length} ${user.likedSongs.length === 1 ? 'song' : 'songs'}`}
+                </span>
+              </span>
             </p>
           </div>
         </div>
         {stations?.map((station) => (
-          <div key={station._id} className={`library-item ${(currentStationId === station._id) && 'current-station'}`} onClick={() => onNavigateToStation(station._id)}>
+          <div
+            key={station._id}
+            className={`library-item ${currentStationId === station._id && 'current-station'}`}
+            onClick={() => onNavigateToStation(station._id)}
+          >
             <button className='library-item__image-button' onClick={() => onPlayStation(station._id)}>
-              <img src={typeof station.imgUrl === 'string' ? station.imgUrl : station.imgUrl[2].url} alt={station.name} />
+              <img
+                src={typeof station.imgUrl === 'string' ? station.imgUrl : station.imgUrl[2].url}
+                alt={station.name}
+              />
               <div className='library-item__image-overlay'>
-                {(currentStationId === station._id && isPlaying) ? <PauseIcon /> : <PlayIcon className='play-icon'/>}
+                {currentStationId === station._id && isPlaying ? <PauseIcon /> : <PlayIcon className='play-icon' />}
               </div>
             </button>
             <div className='library-item__info'>
