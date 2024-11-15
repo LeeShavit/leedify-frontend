a7b94536bcfd84fedfa5a780470cce03bcf9bc19
Lee Shavit <lee.shavit10@gmail.com>
2024-11-16 00:40:53 +0200

    genre page added

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react'
 import exploredata from '../../data/exploredataa.json'
 import { GenreCard } from '../cmps/GenreCard'
+import { useNavigate } from 'react-router'
 
 export function Explore() {
-  const [selectedCategory, setSelectedCategory] = useState('all')
-  const categories = exploredata.categories
+
+  const [selectedGenre, setSelectedGenre] = useState('all')
+  const genres = exploredata.categories
+  const navigate= useNavigate()
+
+  function onSelectGenre(genreId){
+    setSelectedGenre(genreId)
+    navigate(`/genre/${genreId}`)
+  }
 
   return (
     <div className='explore-page'>
       <h1 className='explore-page__title'>Browse All</h1>
       <div className='explore-grid'>
-        {categories.map((category) => (
-          <GenreCard key={category._id} category={category} onSelect={() => setSelectedCategory(category._id)} />
+        {genres.map((genre) => (
+          <GenreCard key={genre._id} genre={genre} onSelect={() => onSelectGenre(genre._id)} />
         ))}
       </div>
     </div>
