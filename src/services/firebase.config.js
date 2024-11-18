72b08fa576af2348b2a99da20147c8cd9100522a
Lee Shavit <lee.shavit10@gmail.com>
2024-11-18 19:35:56 +0200

    update fb creds

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -1,11 +1,18 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { FIREBASE_CONFIG } from './credentials'
+import { FB_API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from './credentials'
 
-const firebaseConf= import.meta.env.FIREBASE_CONFIG || FIREBASE_CONFIG
 
-const config = JSON.parse(firebaseConf)
+export const FIREBASE_CONFIG ={
+    apiKey: import.meta.env.FB_API_KEY || FB_API_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIM || AUTH_DOMAIN,
+    projectId: import.meta.env.PROJECT_ID || PROJECT_ID,
+    storageBucket: import.meta.env.STORAGE_BUCKET || STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID || MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID || APP_ID,
+    measurementId: import.meta.env.MEASUREMENT_ID || MEASUREMENT_ID,
+  }
 
-const app = initializeApp(config)
+const app = initializeApp(FIREBASE_CONFIG)
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
