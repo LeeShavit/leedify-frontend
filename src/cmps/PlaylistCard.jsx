71452c09cba3fa9e58058d34610bcbc7a1af2fb0
Lee Shavit <lee.shavit10@gmail.com>
2024-11-19 14:23:16 +0200

    improve player logic

--- src/cmps/PlaylistCard.jsx
+++ src/cmps/PlaylistCard.jsx
@@ -1,26 +1,24 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { Play, Pause } from 'lucide-react'
-import { addToQueue, clearQueue, playNext, setIsPlaying } from '../store/actions/player.actions'
+import { addToQueue, clearQueue, playNext, replaceQueue, setIsPlaying } from '../store/actions/player.actions'
 import { useSelector } from 'react-redux'
 import { stationService } from '../services/station'
 
 export function PlaylistCard({ station }) {
 
-  const currentStationId = useSelector((state) => state.playerModule.currentStationId)
+  const currentStation = useSelector((state) => state.playerModule.currentStation)
   const isPlaying = useSelector((state) => state.playerModule.isPlaying)
   const navigate = useNavigate()
 
   async function onPlayPauseStation(event) {
     event.stopPropagation()
 
-    if (station._id === currentStationId) {
+    if (station._id === currentStation._id) {
       isPlaying ? setIsPlaying(false) : setIsPlaying(true)
     } else {
       try {
         const { songs } = await stationService.getById(station._id)
-        clearQueue()
-        addToQueue(songs, station._id)
-        playNext()
+        replaceQueue(songs, station)
         setIsPlaying(true)
       } catch (error) {
         console.error('Failed to play playlist:', error)
@@ -33,7 +31,7 @@ export function PlaylistCard({ station }) {
       <div className='playlist-card__image-container'>
         <img src={station.imgUrl} alt={station.name} />
         <button className='playlist-card__play-button' aria-label='Play' onClick={(event) => onPlayPauseStation(event)}>
-          {isPlaying && currentStationId === station._id ? <Pause size={24} color='black' fill='black' /> : <Play size={24} color='black' fill='black' />}
+          {isPlaying && currentStation._id === station._id ? <Pause size={24} color='black' fill='black' /> : <Play size={24} color='black' fill='black' />}
         </button>
       </div>
 
